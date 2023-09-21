e953188deed0280899b7f30512162e0d8bb02f2d
Fatma Nur Kılıçkaya <ftmanurkilickaya@gmail.com>
2023-09-21 16:05:20 +0300

    Tweet app template Inheritance completed and form created

---
+++ djangotweet/tweetapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from . import models
+
+# Create your views here.
+
+def listtweet(request):
+    all_tweets = models.Tweet.objects.all()
+    tweet_dict= {"tweets": all_tweets}
+    
+    return render(request, 'tweetapp/listtweet.html', context=tweet_dict)
+
+def addtweet(request):
+    return render(request, 'tweetapp/addtweet.html')
